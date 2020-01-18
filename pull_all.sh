124be2ec99a19b4df9744821cfcb13152081d603
operant <dave@operant.io>
2020-01-17 20:54:06 -0500

    added git directory check

--- pull_all.sh
+++ pull_all.sh
@@ -4,20 +4,26 @@
 # assumes all subdirectoris in pwd are repos
 
 # move into each repo, git pull
+echo "[.] Starting git pulls..."
 for i in *(/F); do
     pushd $i
-    echo "checking for updates in $i..."
-    pull=$(git pull)
-    if [ $pull = "Already up to date." ]; then
-        print "no updates in $i"
-    elif [ $? -eq 0 ]; then
-        print $pull
-        echo "$i updated successfully."
+    if [ -d ".git" ]; then
+        echo "[.] checking for updates in $i:"
+        pull=$(git pull)
+        if [ $pull = "Already up to date." ]; then
+            print "[.] no updates in $i. \n"
+        elif [ $? -eq 0 ]; then
+            print $pull
+            echo "[.] $i updated successfully. \n"
+        else
+            print "[.] Error pulling $i: " 
+            print $pull "\n"
+            # popd
+        fi
     else
-        print $pull
-        exit 1
+        print "[.] $i is not a git repository. skipping. \n" 
     fi
     popd
 done
 
-echo "done."
+echo "[.] Done."
